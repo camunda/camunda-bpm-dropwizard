03baf0b02a78f8aba9549a8d986ad35e6310f710
Jan Galinski <jan.galinski@holisticon.de>
2014-12-22 10:25:37 +0100

    add delegate

--- examples/src/main/java/org/camunda/bpm/extension/dropwizard/example/CamundaDropwizardExampleApplication.java
+++ examples/src/main/java/org/camunda/bpm/extension/dropwizard/example/CamundaDropwizardExampleApplication.java
@@ -2,23 +2,34 @@ package org.camunda.bpm.extension.dropwizard.example;
 
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
+import io.dropwizard.lifecycle.ServerLifecycleListener;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.extension.dropwizard.CamundaBundle;
 import org.camunda.bpm.extension.dropwizard.healthcheck.CamundaHealthChecks;
+import org.eclipse.jetty.server.Server;
 import org.slf4j.Logger;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class CamundaDropwizardExampleApplication extends Application<CamundaDropwizardExampleApplication.Config> {
 
+    public static final String PROCESS_DEFINITION_KEY = "process_dw_example";
     private final Logger logger = getLogger(this.getClass());
 
     @Override
     public void run(Config configuration, Environment environment) throws Exception {
-        CamundaHealthChecks.processIsDeployed(environment, "process_dw_example");
+        CamundaHealthChecks.processIsDeployed(environment, PROCESS_DEFINITION_KEY);
 
-       // ProcessEngines.getDefaultProcessEngine().getRuntimeService().startProcessInstanceByKey("process_dw_example");
+        environment.lifecycle().addServerLifecycleListener(new ServerLifecycleListener() {
+            @Override
+            public void serverStarted(final Server server) {
+
+                 ProcessEngines.getDefaultProcessEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+            }
+        });
+
     }
 
     @Override
---
+++ examples/src/main/java/org/camunda/bpm/extension/dropwizard/example/delegate/DoSomethingDelegate.java
@@ -0,0 +1,17 @@
+package org.camunda.bpm.extension.dropwizard.example.delegate;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+
+public class DoSomethingDelegate implements JavaDelegate {
+
+    private final Logger logger = getLogger(this.getClass());
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        logger.info("executed delegate: {}", execution);
+    }
+}
